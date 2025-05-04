eb1ec125f0df19ab0edf6fa72650ab7955331170
Hitesh2805 <hiteshmali5206@gmail.com>
2025-05-04 23:10:39 +0530

    Update 8.cpp

--- 8.cpp
+++ 8.cpp
@@ -1,56 +1,137 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
-struct Node {
+struct node {
     int data;
-    Node* left = nullptr;
-    Node* right = nullptr;
-    Node(int val) : data(val) {}
+    node *left;
+    node *right;
 };
 
-Node* insert(Node* root, int value) {
-    if (!root) return new Node(value);
-    if (value < root->data) root->left = insert(root->left, value);
-    else root->right = insert(root->right, value);
-    return root;
+class Bst {
+public:
+    node *root;
+    Bst() {
+        root = NULL;
+    }
+
+    void create();
+    void insert(node*, node*);
+    void dis();
+    void inorder(node*);
+    void preorder(node*);
+    void postorder(node*);
+    int height(node *);
+    void minval(node *);
+};
+
+void Bst::create() {
+    char ch;
+    do {
+        node *nn = new node;
+        cout << "Enter data for node: ";
+        cin >> nn->data;
+        nn->left = NULL;
+        nn->right = NULL;
+        if (root == NULL) {
+            root = nn;
+        } else {
+            insert(root, nn);
+        }
+        cout << "Do you want to add more nodes (y/n)? ";
+        cin >> ch;
+    } while (ch == 'y');
 }
 
-int findMin(Node* root) {
-    while (root && root->left) root = root->left;
-    return root ? root->data : -1;
+void Bst::insert(node *root, node *nn) {
+    if (nn->data < root->data) {
+        if (root->left == NULL) {
+            root->left = nn;
+        } else {
+            insert(root->left, nn);
+        }
+    } else {
+        if (root->right == NULL) {
+            root->right = nn;
+        } else {
+            insert(root->right, nn);
+        }
+    }
 }
 
-int longestPath(Node* root) {
-    if (!root) return 0;
-    return max(longestPath(root->left), longestPath(root->right)) + 1;
+void Bst::inorder(node *root) {
+    if (root == NULL) return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+void Bst::preorder(node *root) {
+    if (root == NULL) return;
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void Bst::postorder(node *root) {
+    if (root == NULL) return;
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+void Bst::dis() {
+    cout << "Inorder: ";
+    inorder(root);
+    cout << "\nPostorder: ";
+    postorder(root);
+    cout << "\nPreorder: ";
+    preorder(root);
+    cout << endl;
+}
+
+int Bst::height(node *root) {
+    if (root == NULL) return 0;
+    int hl = height(root->left);
+    int hr = height(root->right);
+    return (hl > hr ? hl : hr) + 1;
+}
+
+void Bst::minval(node* root) {
+    node *temp = root;
+    while (temp && temp->left != NULL) {
+        temp = temp->left;
+    }
+    if (temp) {
+        cout << "Min value in the BST is: " << temp->data << endl;
+    }
 }
 
 int main() {
-    Node* root = nullptr;
-    int n, value;
-    
-    cout << "Enter number of nodes: ";
-    cin >> n;
-    cout << "Enter the nodes: ";
-    while (n--) {
-        cin >> value;
-        root = insert(root, value);
-    }
-
-    cout << "Enter new value to insert: ";
-    cin >> value;
-    root = insert(root, value);
-
-    cout << "Longest path from root: " << longestPath(root) << endl;
-    cout << "Minimum value in the tree: " << findMin(root) << endl;
+    Bst o;
+    int choice;
+    char ch;
+    do {
+        cout << "1. Insert\n2. Display\n3. Height\n4. Find Min\nEnter your choice: ";
+        cin >> choice;
+        switch (choice) {
+            case 1:
+                o.create();
+                break;
+            case 2:
+                o.dis();
+                break;
+            case 3:
+                cout << "Height of the BST is: " << o.height(o.root) << endl;
+                break;
+            case 4:
+                o.minval(o.root);
+                break;
+            default:
+                cout << "Invalid choice. Please try again.\n";
+        }
+        cout << "Do you want to continue (y/n)? ";
+        cin >> ch;
+    } while (ch == 'y');
 
     return 0;
 }
-
-// OUTPUT
-Enter number of nodes: 5
-Enter the nodes: 20 8 25 4 12
-Enter new value to insert: 10
-Longest path from root: 4
-Minimum value in the tree: 4
