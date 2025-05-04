c5ff9f6ce46ef66a9aff2d083b3074cdb3f9dc93
Hitesh2805 <hiteshmali5206@gmail.com>
2025-05-04 23:36:49 +0530

    Update 15.cpp

--- 15.cpp
+++ 15.cpp
@@ -1,42 +1,99 @@
 #include <iostream>
 using namespace std;
 
-struct Node {
-    int key;
-    Node *left, *right;
-    Node(int k) : key(k), left(0), right(0) {}
+class OptimalBST {
+public:
+    int *keys;
+    float *probabilities;
+    int n;
+
+    OptimalBST(int n) {
+        this->n = n;
+        keys = new int[n];
+        probabilities = new float[n];
+    }
+
+    void input() {
+        cout << "Enter keys: ";
+        for (int i = 0; i < n; i++) {
+            cin >> keys[i];
+        }
+
+        cout << "Enter probabilities: ";
+        for (int i = 0; i < n; i++) {
+            cin >> probabilities[i];
+        }
+    }
+
+    void computeOptimalBST() {
+        float **cost = new float*[n+1];
+        int **root = new int*[n+1];
+
+        for (int i = 0; i <= n; i++) {
+            cost[i] = new float[n+1];
+            root[i] = new int[n+1];
+        }
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
+                cost[i][j] = 0;
+                root[i][j] = -1;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            cost[i][i] = probabilities[i];
+            root[i][i] = i;
+        }
+
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i < n - len + 1; i++) {
+                int j = i + len - 1;
+                cost[i][j] = 1e9; 
+
+                float sum = 0;
+                for (int k = i; k <= j; k++) {
+                    sum += probabilities[k];
+                }
+
+                for (int r = i; r <= j; r++) {
+                    float temp = (r > i ? cost[i][r-1] : 0) + (r < j ? cost[r+1][j] : 0) + sum;
+                    if (temp < cost[i][j]) {
+                        cost[i][j] = temp;
+                        root[i][j] = r;
+                    }
+                }
+            }
+        }
+
+        cout << "Minimum Search Cost: " << cost[0][n-1] << endl;
+
+        cout << "Optimal BST Root Positions: " << endl;
+        printOptimalBST(root, 0, n-1);
+    }
+
+    void printOptimalBST(int **root, int i, int j) {
+        if (i > j) return;
+        int r = root[i][j];
+        cout << "Root: " << keys[r] << endl;
+        printOptimalBST(root, i, r-1);
+        printOptimalBST(root, r+1, j);
+    }
+
+    ~OptimalBST() {
+        delete[] keys;
+        delete[] probabilities;
+    }
 };
 
-Node* build(int a[], int l, int r) {
-    if (l > r) return 0;
-    int m = (l + r) / 2;
-    Node* root = new Node(a[m]);
-    root->left = build(a, l, m - 1);
-    root->right = build(a, m + 1, r);
-    return root;
-}
-
-void preorder(Node* r) {
-    if (!r) return;
-    cout << r->key << " ";
-    preorder(r->left);
-    preorder(r->right);
-}
-
 int main() {
-    int n, a[100];
-    cout << "Enter number of keys: ";
+    int n;
+    cout << "Enter the number of keys: ";
     cin >> n;
-    cout << "Enter sorted keys:\n";
-    for (int i = 0; i < n; i++) cin >> a[i];
-    Node* root = build(a, 0, n - 1);
-    cout << "Balanced BST (Preorder): ";
-    preorder(root);
+
+    OptimalBST tree(n);
+    tree.input();
+    tree.computeOptimalBST();
+
+    return 0;
 }
-
-/// OUTPUT
-
-Enter number of keys: 5
-Enter sorted keys:
-10 20 30 40 50
-Balanced BST (Preorder): 30 10 20 40 50 
