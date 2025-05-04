d24c3bde88cc6390dea2fdac966f690c5a74397f
Hitesh2805 <hiteshmali5206@gmail.com>
2025-05-04 23:40:43 +0530

    Update 17.cpp

--- 17.cpp
+++ 17.cpp
@@ -1,38 +1,64 @@
 #include <iostream>
+#define max 100
 using namespace std;
-
-void heapify(int a[], int n, int i, bool maxHeap) {
-    int ext = i, l = 2*i+1, r = 2*i+2;
-    if (l < n && ((maxHeap && a[l] > a[ext]) || (!maxHeap && a[l] < a[ext]))) ext = l;
-    if (r < n && ((maxHeap && a[r] > a[ext]) || (!maxHeap && a[r] < a[ext]))) ext = r;
-    if (ext != i) {
-        swap(a[i], a[ext]);
-        heapify(a, n, ext, maxHeap);
+class mar
+{
+public:
+    int arr[max], temp, n, left, right, largest,z;
+    void create();
+    void hfy(int);
+};
+void mar::hfy(int i)
+{
+    right = 2 * i + 2;
+    left = 2 * i + 1;
+    largest = i;
+    if (left < n && arr[left] > arr[largest])
+    {
+        largest = left;
+    }
+    if (right < n && arr[right] > arr[largest])
+    {
+        largest = right;
+    }
+    if (i != largest)
+    {
+        temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+        hfy(largest);
     }
 }
-
-void buildHeap(int a[], int n, bool maxHeap) {
-    for (int i = n/2 - 1; i >= 0; i--)
-        heapify(a, n, i, maxHeap);
-}
-
-int main() {
-    int n, a[100];
-    cout << "Enter number of students: ";
+void mar::create()
+{
+    cout << "Enter the number of students:=" << endl;
     cin >> n;
-    cout << "Enter marks: ";
-    for (int i = 0; i < n; i++) cin >> a[i];
-
-    buildHeap(a, n, true);  // max-heap
-    cout << "Max Marks = " << a[0] << endl;
-
-    buildHeap(a, n, false); // min-heap
-    cout << "Min Marks = " << a[0] << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter the marks for student no." << i + 1 << " :=" << endl;
+        cin >> arr[i];
+    }
+        for (int i = (n / 2) - 1; i >= 0; i--)
+        {
+            hfy(i);
+        }
+        z=n;
+    for(int i=n-1;i>=0;i--){
+        temp=arr[i];
+        arr[i]=arr[0];
+        arr[0]=temp;
+        n--;
+        hfy(0);
+    }     
+    cout<<"Maximum marks obtained in the subject:="<<endl;
+    cout<<arr[z-1]<<endl;
+    cout<<"Minimum marks obtained in the subject:="<<endl;
+    cout<<arr[0]<<endl;
+    
 }
-
-// OUTPUT
-
-Enter number of students: 5
-Enter marks: 12 9 18 45 7
-Max Marks = 45
-Min Marks = 7
+int main()
+{
+    mar ab;
+    ab.create();
+    return 0;
+}
